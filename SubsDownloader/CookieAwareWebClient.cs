0074f3ebf6f10b128b80b3a75caf9a4a9d6ea697
thefluxcapacitor <thefluxcapacitor@thefluxcapacitor.com>
2013-06-07 18:28:44 -0300

    soporte para login en subdivx

---
+++ SubsDownloader/CookieAwareWebClient.cs
@@ -0,0 +1,27 @@
+﻿namespace SubsDownloader
+{
+    using System;
+    using System.Net;
+
+    public class CookieAwareWebClient : WebClient
+    {
+        private readonly CookieContainer container;
+
+        public CookieAwareWebClient(CookieContainer container)
+        {
+            this.container = container;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address);
+            var webRequest = request as HttpWebRequest;
+            if (webRequest != null)
+            {
+                webRequest.CookieContainer = this.container;
+            }
+
+            return request;
+        }
+    }
+}
--- SubsDownloader/SubDivXManager.cs
+++ SubsDownloader/SubDivXManager.cs
@@ -2,23 +2,30 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Configuration;
     using System.IO;
     using System.Linq;
     using System.Net;
+    using System.Text;
     using System.Web;
 
     using HtmlAgilityPack;
 
     public class SubDivXManager
     {
+        private CookieContainer loginCookies = new CookieContainer();
+
         public IList<Sub> GetCandidateSubs(Video video, bool searchInComments)
         {
             Console.WriteLine();
             Console.WriteLine("Text used to seach subtitle is: {0}", video.GetSearchString());
             Console.WriteLine("Release Group to match: {0}", video.ReleaseGroup);
 
+            this.login();
+
             var candidateSubs = new List<Sub>();
-            var subdivxClient = new WebClient();
+            var subdivxClient = new CookieAwareWebClient(this.loginCookies);
             subdivxClient.Headers["User-Agent"] = "Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))";
 
             var page = 1;
@@ -26,8 +33,9 @@
             while (page > 0)
             {
                 var url = this.GetUrl(video.GetSearchString(), page);
-                var output = subdivxClient.DownloadString(url);
 
+                var output = subdivxClient.DownloadString(url);
+                
                 var doc = new HtmlDocument();
                 doc.LoadHtml(output);
                 var subsHtmlNodes = doc.DocumentNode.Descendants("div").Where(div => div.GetAttributeValue("id", string.Empty).Equals("buscador_detalle"));
@@ -63,6 +71,39 @@
             return candidateSubs;
         }
 
+        private void login()
+        {
+            var encoding = new ASCIIEncoding();
+
+            var values = new NameValueCollection();
+            values.Add("usuario", ConfigurationManager.AppSettings["subdivxUserName"]);
+            values.Add("clave", ConfigurationManager.AppSettings["subdivxPassword"]);
+            values.Add("Enviar", "Entrar");
+            values.Add("accion", "14");
+            values.Add("enviau", "1");
+
+            var array = (from key in values.AllKeys
+                         from value in values.GetValues(key)
+                         select string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(value)))
+                .ToArray();
+
+            byte[] data = encoding.GetBytes(string.Join("&", array));
+
+            var myRequest = (HttpWebRequest)WebRequest.Create("http://www.subdivx.com/index.php");
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.ContentLength = data.Length;
+            var newStream = myRequest.GetRequestStream();
+            newStream.Write(data, 0, data.Length);
+            newStream.Close();
+
+            var response = myRequest.GetResponse();
+
+            var cookies = response.Headers["Set-Cookie"];
+
+            this.loginCookies.SetCookies(new Uri("http://www.subdivx.com/index.php"), cookies);
+        }
+
         private string GetSubComments(WebClient webClient, HtmlNode subHtmlNode)
         {
             var aux = subHtmlNode.InnerHtml;
@@ -90,7 +131,7 @@
 
         public void DownloadSub(string path, string downloadUrl)
         {
-            var subdivxClient = new WebClient();
+            var subdivxClient = new CookieAwareWebClient(this.loginCookies);
             subdivxClient.Headers["User-Agent"] = "Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))";
 
             var directory = Path.GetDirectoryName(path);
